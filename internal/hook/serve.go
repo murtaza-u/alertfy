9ca607b14008d861da88358023a6d1f72e12acad
Murtaza Udaipurwala <murtaza@murtazau.xyz>
2024-08-13 23:21:09 +0530

    feat: define alertmanager request data

--- internal/hook/serve.go
+++ internal/hook/serve.go
@@ -2,11 +2,31 @@ package hook
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/labstack/echo/v4"
 )
 
+type request struct {
+	Receiver string  `json:"receiver"`
+	Status   string  `json:"status"`
+	Alerts   []alert `json:"alerts"`
+}
+
+type alert struct {
+	Status       string            `json:"status"`
+	Labels       map[string]string `json:"labels"`
+	Annotations  map[string]string `json:"annotations"`
+	StartsAt     time.Time         `json:"startsAt"`
+	EndsAt       time.Time         `json:"endsAt"`
+	GeneratorURL string            `json:"generatorURL"`
+	Fingerprint  string            `json:"fingerprint"`
+}
+
 func (h Hook) serve(c echo.Context) error {
-	c.NoContent(http.StatusAccepted)
-	return nil
+	req := new(request)
+	if err := c.Bind(req); err != nil {
+		return c.NoContent(http.StatusBadRequest)
+	}
+	return c.NoContent(http.StatusAccepted)
 }
